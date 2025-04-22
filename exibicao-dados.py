ef0624d02b46b8736d57471a2dc8b52448f2435b
Enzo Olivato <enzopazian1402@gmail.com>
2025-04-22 18:45:42 -0300

    Fase 2 - versao final para a entrega

--- exibicao-dados.py
+++ exibicao-dados.py
@@ -9,20 +9,15 @@ connection = mysql.connector.connect(
 )
 cursor = connection.cursor()
 
-# Criando a tabela utilizada nas consultas
-# comando = CREATE TABLE `pi_entradas_sustentabilidade` ('id' int NOT NULL AUTO_INCREMENT, 'data_registro' date NOT NULL, 'consumo_agua' float NOT NULL, 'consumo_energia' float NOT NULL, 'pct_lixo' float NOT NULL, 'transporte_publico' varchar(1) NOT NULL, 'bicicleta' varchar(1) NOT NULL, 'caminhada' varchar(1) NOT NULL, 'carro_comb_fossil' varchar(1) NOT NULL, 'carro_eletrico' varchar(1) NOT NULL, 'carona' varchar(1) NOT NULL, PRIMARY KEY ('id'))
-# cursor.execute(comando)
-# connection.commit()
-
 # Exibição dos dados armazenados
 comando = 'SELECT * FROM pi_entradas_sustentabilidade'
 cursor.execute(comando)
 resultado = []
 resultado = cursor.fetchall() # lê o banco de dados -> apenas para selects
 print('\nExibição dos dados de inserção:\n')
-print('-'*162)
+print('='*162)
 print('| ID | Data de registro | Consumo de água | Consumo de energia | Quantidade de lixo gerado | % de lixo reciclável gerado | Transportes utilizados\t\t |')
-print('-'*162)
+print('='*162)
 for i in range(len(resultado)):
     print(f'  {resultado[i][0]}', end='\t')
     print(resultado[i][1], end='\t   ')
@@ -31,7 +26,7 @@ for i in range(len(resultado)):
     print(f'{resultado[i][4]} Kg', end='\t\t\t')
     print(f'{resultado[i][5]}%', end='\t\t\t\t')
     print(resultado[i][6:12])
-print('-'*162)
+    print('-'*162)
 
 # Exibição da classificação dos registros
 comando = 'SELECT * FROM pi_classificacoes_sustentabilidade'
@@ -39,37 +34,36 @@ cursor.execute(comando)
 resultado_class = []
 resultado_class = cursor.fetchall()
 print('\nExibição da classificação dos dados armazenados:\n')
-print('-'*162)
+print('='*129)
 print('| ID | Consumo de água\t\t| Consumo de energia\t\t| % de lixo reciclável gerado\t| Transportes utilizados\t|')
-print('-'*162)
+print('='*129)
 for i in range(len(resultado_class)):
     print(f'  {resultado_class[i][0]}', end='\t')
     print(resultado_class[i][1], end='\t   ')
     print(resultado_class[i][2], end='\t   ')
     print(resultado_class[i][3], end='\t   ')
     print(resultado_class[i][4])
-print('-'*162)
+    print('-'*129)
 
 # Processamento dos dados armazenados
 print('\nProcessamento dos dados armazenados:\n')
-print('-'*162)
+print('='*129)
 print('Média dos dados coletados')
-print('-'*162)
+print('='*129)
 
 # Água
 comando = 'SELECT avg(consumo_agua) FROM pi_entradas_sustentabilidade'
 cursor.execute(comando)
 media_agua = cursor.fetchone()
-print(f'Média do consumo de água: {media_agua[0]:.2f} L')
+print(f'\nMédia do consumo de água: {media_agua[0]:.2f} L')
 
 if (media_agua[0] >= 150):
     if (media_agua[0] > 200):
-        print("Classificação do consumo de água: Baixa sustentabilidade")
+        print("Classificação do consumo de água: Baixa sustentabilidade\n")
     else:
-        print("Classificação do consumo de água: Moderada sustentabilidade")
+        print("Classificação do consumo de água: Moderada sustentabilidade\n")
 else:
-    print("Classificação do consumo de água: Alta sustentabilidade")
-print('\n')
+    print("Classificação do consumo de água: Alta sustentabilidade\n")
 
 # Energia
 comando = 'SELECT avg(consumo_energia) FROM pi_entradas_sustentabilidade'
@@ -79,12 +73,11 @@ media_energia = cursor.fetchone()
 print(f'Média do consumo de energia: {media_energia[0]:.2f} kwH')
 if (media_energia[0] >= 5): 
     if (media_energia[0] > 10):
-        print("Classificação do consumo de energia: Baixa sustentabilidade")
+        print("Classificação do consumo de energia: Baixa sustentabilidade\n")
     else: 
-        print("Classificação do consumo de energia: Moderada sustentabilidade")
+        print("Classificação do consumo de energia: Moderada sustentabilidade\n")
 else:
-    print("Classificação do consumo de energia: Alta sustentabilidade")   
-print('\n')
+    print("Classificação do consumo de energia: Alta sustentabilidade\n")   
 
 #Porcentagem
 comando = 'SELECT avg(pct_lixo) FROM pi_entradas_sustentabilidade'
@@ -94,12 +87,11 @@ print(f'Média da porcentagem de lixo reciclável gerado: {media_lixo[0]:.2f}%')
 
 if (media_lixo[0] <= 50):
     if (media_lixo[0] < 20):
-        print("Classificação da geração de resíduos recicláveis: Baixa sustentabilidade")
+        print("Classificação da geração de resíduos recicláveis: Baixa sustentabilidade\n")
     else:
-        print("Classificação da geração de resíduos recicláveis: Moderada sustentabilidade")
+        print("Classificação da geração de resíduos recicláveis: Moderada sustentabilidade\n")
 else:
-    print("Classificação da geração de resíduos recicláveis: Alta sustentabilidade")
-print('\n')
+    print("Classificação da geração de resíduos recicláveis: Alta sustentabilidade\n")
 
 # Tipos de transporte
 comando = 'SELECT transporte_publico, bicicleta, caminhada, carro_comb_fossil, carro_eletrico, carona FROM pi_entradas_sustentabilidade'
@@ -124,7 +116,6 @@ elif (not 1 in class_transporte) and (not 2 in class_transporte):
     print('Baixa sustentabilidade')
 else:
     print('Moderada sustentabilidade')
-print('-'*162)
 
 cursor.close()
 connection.close()
