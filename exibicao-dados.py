136a9cef6a3750027841461042c49e42ebbd94f3
Enzo Olivato <enzopazian1402@gmail.com>
2025-04-23 21:03:36 -0300

    Fase 2 - Exibição das tabelas aprimorada

--- exibicao-dados.py
+++ exibicao-dados.py
@@ -15,17 +15,24 @@ cursor.execute(comando)
 resultado = []
 resultado = cursor.fetchall() # lê o banco de dados -> apenas para selects
 print('\nExibição dos dados de inserção:\n')
+# Escrevendo a tabela
 print('='*162)
-print('| ID | Data de registro | Consumo de água | Consumo de energia | Quantidade de lixo gerado | % de lixo reciclável gerado | Transportes utilizados\t\t |')
+print(f'{"| ID ":^5}', end='')
+print(f'{"| Data de registro ":^15}', end='')
+print(f'{"| Consumo de água ":^18}', end='')
+print(f'{"| Consumo de energia ":^21}', end='')
+print(f'{"| Quantidade de lixo gerado ":^28}', end='')
+print(f'{"| % de lixo reciclável gerado ":^30}', end='')
+print(f'{"| Meios de transportes utilizados":<40}', end=f'{"|"}\n')
 print('='*162)
 for i in range(len(resultado)):
-    print(f'  {resultado[i][0]}', end='\t')
-    print(resultado[i][1], end='\t   ')
-    print(f'{resultado[i][2]} L', end='\t\t')
-    print(f'{resultado[i][3]} KwH', end='\t\t\t')
-    print(f'{resultado[i][4]} Kg', end='\t\t\t')
-    print(f'{resultado[i][5]}%', end='\t\t\t\t')
-    print(resultado[i][6:12])
+    print(f'|{resultado[i][0]:^4}', end='|')
+    print(f'{str(resultado[i][1]):^18}', end='|')
+    print(f'{f"{resultado[i][2]} L":^17}', end='|')
+    print(f'{f"{resultado[i][3]} KwH":^20}', end='|')
+    print(f'{f"{resultado[i][4]} Kg":^27}', end='|')
+    print(f'{f"{resultado[i][5]}%":^29}', end='| ')
+    print(resultado[i][6:12], end=f'{"|":>9}\n')
     print('-'*162)
 
 # Exibição da classificação dos registros
@@ -34,22 +41,26 @@ cursor.execute(comando)
 resultado_class = []
 resultado_class = cursor.fetchall()
 print('\nExibição da classificação dos dados armazenados:\n')
-print('='*129)
-print('| ID | Consumo de água\t\t| Consumo de energia\t\t| % de lixo reciclável gerado\t| Transportes utilizados\t|')
-print('='*129)
+print('='*143)
+print(f'{"| ID ":^5}', end='|')
+print(f'{"Consumo de água ":^30}', end='|')
+print(f'{"Consumo de energia ":^30}', end='|')
+print(f'{"% de lixo reciclável gerado ":^32}', end='|')
+print(f'{"Meios de transportes utilizados":^41}', end=f'{"|"}\n')
+print('='*143)
 for i in range(len(resultado_class)):
-    print(f'  {resultado_class[i][0]}', end='\t')
-    print(resultado_class[i][1], end='\t   ')
-    print(resultado_class[i][2], end='\t   ')
-    print(resultado_class[i][3], end='\t   ')
-    print(resultado_class[i][4])
-    print('-'*129)
+    print(f'|{resultado_class[i][0]:^4}', end='|')
+    print(f'{f"{resultado_class[i][1]}":^30}', end='|')
+    print(f'{f"{resultado_class[i][2]}":^30}', end='|')
+    print(f'{f"{resultado_class[i][3]}":^32}', end='| ')
+    print(f'{resultado_class[i][4]:^40}', end='|\n')
+    print('-'*143)
 
 # Processamento dos dados armazenados
 print('\nProcessamento dos dados armazenados:\n')
-print('='*129)
+print('='*143)
 print('Média dos dados coletados')
-print('='*129)
+print('='*143)
 
 # Água
 comando = 'SELECT avg(consumo_agua) FROM pi_entradas_sustentabilidade'
@@ -64,26 +75,28 @@ if (media_agua[0] >= 150):
         print("Classificação do consumo de água: Moderada sustentabilidade\n")
 else:
     print("Classificação do consumo de água: Alta sustentabilidade\n")
+print('-'*143)
 
 # Energia
 comando = 'SELECT avg(consumo_energia) FROM pi_entradas_sustentabilidade'
 cursor.execute(comando)
 media_energia = cursor.fetchone()
 
-print(f'Média do consumo de energia: {media_energia[0]:.2f} kwH')
+print(f'\nMédia do consumo de energia: {media_energia[0]:.2f} kwH')
 if (media_energia[0] >= 5): 
     if (media_energia[0] > 10):
         print("Classificação do consumo de energia: Baixa sustentabilidade\n")
     else: 
         print("Classificação do consumo de energia: Moderada sustentabilidade\n")
 else:
-    print("Classificação do consumo de energia: Alta sustentabilidade\n")   
+    print("Classificação do consumo de energia: Alta sustentabilidade\n")  
+print('-'*143) 
 
 #Porcentagem
 comando = 'SELECT avg(pct_lixo) FROM pi_entradas_sustentabilidade'
 cursor.execute(comando)
 media_lixo = cursor.fetchone()
-print(f'Média da porcentagem de lixo reciclável gerado: {media_lixo[0]:.2f}%')
+print(f'\nMédia da porcentagem de lixo reciclável gerado: {media_lixo[0]:.2f}%')
 
 if (media_lixo[0] <= 50):
     if (media_lixo[0] < 20):
@@ -92,6 +105,7 @@ if (media_lixo[0] <= 50):
         print("Classificação da geração de resíduos recicláveis: Moderada sustentabilidade\n")
 else:
     print("Classificação da geração de resíduos recicláveis: Alta sustentabilidade\n")
+print('-'*143)
 
 # Tipos de transporte
 comando = 'SELECT transporte_publico, bicicleta, caminhada, carro_comb_fossil, carro_eletrico, carona FROM pi_entradas_sustentabilidade'
@@ -108,14 +122,15 @@ for i in range(2, len(dados_transporte)-1):
     else:
             class_transporte.append(3)
 
-print('Média da classificação de transportes: ', end='')
+print('\nMédia da classificação de transportes: ', end='')
 # Análise dos dados convertidos
 if (not 3 in class_transporte) and (not 2 in class_transporte):
-    print('Alta sustentabilidade')
+    print('Alta sustentabilidade\n')
 elif (not 1 in class_transporte) and (not 2 in class_transporte):
-    print('Baixa sustentabilidade')
+    print('Baixa sustentabilidade\n')
 else:
-    print('Moderada sustentabilidade')
+    print('Moderada sustentabilidade\n')
+print('-'*143)
 
 cursor.close()
 connection.close()
