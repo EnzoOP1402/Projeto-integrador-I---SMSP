ec47f7416c578f552cee412a3ef50cb695f2c9b5
Enzo Olivato <enzopazian1402@gmail.com>
2025-04-29 11:30:06 -0300

    Primeira etapa da terceira fase - menu criado e listagens aplicadas

---
+++ sistema-de-monitoramento-sustentavel.py
@@ -0,0 +1,230 @@
+# Importando a blioteca para obter a data atual
+from datetime import date
+
+# Importando o conector sql
+import mysql.connector
+
+# Criando a conexão -> adicionar as infromações do seu banco de dados
+connection = mysql.connector.connect(
+    host = '',
+    user='',
+    password='',
+    database=''
+)
+
+cursor = connection.cursor()
+
+print('-'*162)
+print(f'{">=<>=<>=< BEM VINDO(A) AO SISTEMA DE MONITORAMENTO DE SUSTENTABILIDADE PESSOAL >=<>=<>=<":^162}')
+print('-'*162)
+
+# Looping de funcionamento do programa
+while True:
+
+    # Exibição do menu
+    menu = int(input('\nSelecione a opção desejada:\n\n[1] - Inserir dados\n[2] - Alterar dados\n[3] - Apagar dados\n[4] - Listar registros\n[5] - Listar médias\n[6] - Sair\n\n>> '))
+    match menu:
+        # Inserção de dados
+        case 1:
+            data = date.today()
+
+            consumo_agua = float(input("\nInforme o quanto de água você consumiu hoje (Aprox., em Litros): "))
+
+            consumo_energia = float(input("\nInforme quanto de energia elétrica você consumiu hoje (em kWh): "))
+
+            residuos_nao_reciclaveis = float(input("\nInforme quantos Kg de resíduos não recicláveis você produziu hoje: ")) 
+
+            residuos_reciclaveis = float(input("\nInforme a porcentagem de resíduos recicláveis que você gerou hoje: "))
+
+            #Inserção e validação inicial dos dados de transporte
+
+            print("Indique quais meios de transporte foram utilizados por você hoje\n")
+
+            while True:
+                transporte_publico = str(input("Transporte público (ônibus, metrô ou trem) [S/N]\n>> ")).upper()
+                if transporte_publico == 'S' or transporte_publico == 'N':
+                    break
+                else:
+                    print("Resposta inválida\n")
+
+            while True:
+                bicicleta = str(input("Bicicleta [S/N]\n>> ")).upper()
+                if bicicleta == 'S' or bicicleta == 'N':
+                    break
+                else:
+                    print("Resposta inválida\n")
+                    
+            while True: 
+                caminhada = str(input("Caminhada [S/N]\n>> ")).upper()
+                if caminhada == 'S' or caminhada == 'N':
+                    break
+                else:
+                    print("Resposta inválida\n")
+
+            while True:
+                carro_combustivel = str(input("Carro (combustível fóssil)? [S/N]\n>> ")).upper()
+                if carro_combustivel == 'S' or carro_combustivel == 'N':
+                    break
+                else:
+                    print("Resposta inválida\n")
+
+            while True:
+                carro_eletrico = str(input("Carro elétrico? [S/N]\n>> ")).upper()
+                if carro_eletrico == 'S' or carro_eletrico == 'N':
+                    break
+                else:
+                    print("Resposta inválida\n")
+
+            while True:
+                carona_compartilhada = str(input("Carona compartilhada (combustível fóssil) [S/N]\n>> ")).upper()
+                if carona_compartilhada == 'S' or carona_compartilhada == 'N':
+                    break
+                else:
+                    print("Resposta inválida\n")
+                
+        # Alteração de dados
+        case 2:
+            print('Alterou')
+
+        # Apagamento de dados
+        case 3:
+            print('apagou')
+
+        # Listagem de registros
+        case 4:
+            # Exibição dos dados armazenados
+            comando = 'SELECT * FROM pi_entradas_sustentabilidade'
+            cursor.execute(comando)
+            resultado = []
+            resultado = cursor.fetchall() # lê o banco de dados -> apenas para selects
+            print('\nExibição dos dados de inserção:\n')
+            # Escrevendo a tabela
+            print('='*162)
+            print(f'{"| ID ":^5}', end='')
+            print(f'{"| Data de registro ":^15}', end='')
+            print(f'{"| Consumo de água ":^18}', end='')
+            print(f'{"| Consumo de energia ":^21}', end='')
+            print(f'{"| Quantidade de lixo gerado ":^28}', end='')
+            print(f'{"| % de lixo reciclável gerado ":^30}', end='')
+            print(f'{"| Meios de transportes utilizados":<40}', end=f'{"|"}\n')
+            print('='*162)
+            for i in range(len(resultado)):
+                print(f'|{resultado[i][0]:^4}', end='|')
+                print(f'{str(resultado[i][1]):^18}', end='|')
+                print(f'{f"{resultado[i][2]} L":^17}', end='|')
+                print(f'{f"{resultado[i][3]} KwH":^20}', end='|')
+                print(f'{f"{resultado[i][4]} Kg":^27}', end='|')
+                print(f'{f"{resultado[i][5]}%":^29}', end='| ')
+                print(resultado[i][6:12], end=f'{"|":>9}\n')
+                print('-'*162)
+
+            # Exibição da classificação dos registros
+            comando = 'SELECT * FROM pi_classificacoes_sustentabilidade'
+            cursor.execute(comando)
+            resultado_class = []
+            resultado_class = cursor.fetchall()
+            print('\nExibição da classificação dos dados armazenados:\n')
+            print('='*143)
+            print(f'{"| ID ":^5}', end='|')
+            print(f'{"Consumo de água ":^30}', end='|')
+            print(f'{"Consumo de energia ":^30}', end='|')
+            print(f'{"% de lixo reciclável gerado ":^32}', end='|')
+            print(f'{"Meios de transportes utilizados":^41}', end=f'{"|"}\n')
+            print('='*143)
+            for i in range(len(resultado_class)):
+                print(f'|{resultado_class[i][0]:^4}', end='|')
+                print(f'{f"{resultado_class[i][1]}":^30}', end='|')
+                print(f'{f"{resultado_class[i][2]}":^30}', end='|')
+                print(f'{f"{resultado_class[i][3]}":^32}', end='| ')
+                print(f'{resultado_class[i][4]:^40}', end='|\n')
+                print('-'*143)
+
+        # Listagem de médias
+        case 5:
+            print('='*143)
+            print('Média dos dados coletados')
+            print('='*143)
+
+            # Água
+            comando = 'SELECT avg(consumo_agua) FROM pi_entradas_sustentabilidade'
+            cursor.execute(comando)
+            media_agua = cursor.fetchone()
+            print(f'\nMédia do consumo de água: {media_agua[0]:.2f} L')
+
+            if (media_agua[0] >= 150):
+                if (media_agua[0] > 200):
+                    print("Classificação do consumo de água: Baixa sustentabilidade\n")
+                else:
+                    print("Classificação do consumo de água: Moderada sustentabilidade\n")
+            else:
+                print("Classificação do consumo de água: Alta sustentabilidade\n")
+            print('-'*143)
+
+            # Energia
+            comando = 'SELECT avg(consumo_energia) FROM pi_entradas_sustentabilidade'
+            cursor.execute(comando)
+            media_energia = cursor.fetchone()
+
+            print(f'\nMédia do consumo de energia: {media_energia[0]:.2f} kwH')
+            if (media_energia[0] >= 5): 
+                if (media_energia[0] > 10):
+                    print("Classificação do consumo de energia: Baixa sustentabilidade\n")
+                else: 
+                    print("Classificação do consumo de energia: Moderada sustentabilidade\n")
+            else:
+                print("Classificação do consumo de energia: Alta sustentabilidade\n")  
+            print('-'*143) 
+
+            #Porcentagem
+            comando = 'SELECT avg(pct_lixo) FROM pi_entradas_sustentabilidade'
+            cursor.execute(comando)
+            media_lixo = cursor.fetchone()
+            print(f'\nMédia da porcentagem de lixo reciclável gerado: {media_lixo[0]:.2f}%')
+
+            if (media_lixo[0] <= 50):
+                if (media_lixo[0] < 20):
+                    print("Classificação da geração de resíduos recicláveis: Baixa sustentabilidade\n")
+                else:
+                    print("Classificação da geração de resíduos recicláveis: Moderada sustentabilidade\n")
+            else:
+                print("Classificação da geração de resíduos recicláveis: Alta sustentabilidade\n")
+            print('-'*143)
+
+            # Tipos de transporte
+            comando = 'SELECT transporte_publico, bicicleta, caminhada, carro_comb_fossil, carro_eletrico, carona FROM pi_entradas_sustentabilidade'
+            cursor.execute(comando)
+            dados_transporte = class_transporte = []
+            dados_transporte = cursor.fetchall()
+            # Conversão das letras de cada linha em classificação (1 - alta; 2 - moderada; 3 - baixa)
+            for i in range(2, len(dados_transporte)-1):
+                if dados_transporte[i][0] == 'S' or dados_transporte[i][1] == 'S' or dados_transporte[i][2] == 'S' or dados_transporte[i][4] == 'S':
+                    if dados_transporte[i][3] == 'S' or dados_transporte[i][5] == 'S':
+                        class_transporte.append(2)
+                    else:
+                        class_transporte.append(1)
+                else:
+                        class_transporte.append(3)
+
+            print('\nMédia da classificação de transportes: ', end='')
+            # Análise dos dados convertidos
+            if (not 3 in class_transporte) and (not 2 in class_transporte):
+                print('Alta sustentabilidade\n')
+            elif (not 1 in class_transporte) and (not 2 in class_transporte):
+                print('Baixa sustentabilidade\n')
+            else:
+                print('Moderada sustentabilidade\n')
+            print('-'*143)
+
+        # Sair do programa
+        case 6:
+            break
+
+        # Opções inválidas
+        case _:
+            print('Opção inválida! Tente novamente.\n')
+
+cursor.close()
+connection.close()
+
+print('Encerrando programa...')
+print('ATÉ A PRÓXIMA!')
